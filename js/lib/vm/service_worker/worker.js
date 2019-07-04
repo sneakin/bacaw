dde898753cc7ba5508fc7afa01d05ea2f2e5e4bb
Nolan Eakins <sneakin@semanticgap.com>
2019-07-04 04:59:11 -0400

    Offline Cacher service worker done right...maybe.

--- js/lib/vm/service_worker/worker.js
+++ js/lib/vm/service_worker/worker.js
@@ -1,8 +1,21 @@
+const moreutil = require('more_util');
+const Cacher = require('vm/service_worker/offline_cacher');
+
 function Worker()
 {
 }
 
-Worker.install = function(self) {
+Worker.Defaults = {
+  cacher: {
+    fetcher: Cacher.fetch_first
+  }
+};
+
+Worker.install = function(self, options) {
+  options = moreutil.merge_options(Worker.Defaults, options);
+
+  Cacher.install(self, options.cacher);
+
   self.addEventListener('install', (event) => {
     console.log("VM worker installing");
   });
@@ -11,7 +24,7 @@ Worker.install = function(self) {
     console.log("VM worker activated");
   });
 
-  // messages
+  // messages?
 }
 
 if(typeof(module) != 'undefined') {
--- www/service_worker.js
+++ www/service_worker.js
@@ -12,53 +12,24 @@ const manifest = [
   './index.css'
 ];
 
-VMWorker.install(self);
+VMWorker.install(self, {
+  cacher: {
+    version: Version,
+    old_version: OldVersion,
+    manifest: manifest
+  }
+});
 
 self.addEventListener('install', (event) => {
   console.log("Service worker installing");
-  event.waitUntil(
-    caches.open(Version).then((cache) => {
-      return cache.addAll(manifest);
-    }).catch((error) => {
-      console.log("Service worker failed to cache", error, error.code, error.message, error.name);
-    }));
 });
 
 self.addEventListener('activate', (event) => {
   console.log("Service worker activated");
-  caches.has(OldVersion).then(() => {
-    caches.delete(OldVersion).then(() => {
-      console.log("Deleted old cache");
-    });
-  });
 });
 
 self.addEventListener('fetch', (event) => {
   console.log("Service worker fetching", event.request);
-  event.respondWith(caches.match(event.request).then((response) => {
-    if(response !== undefined) {
-      return response;
-    } else {
-      return fetch(event.request).then((response) => {
-        var r = response.clone();
-        caches.open(Version).then((cache) => {
-          cache.put(event.request, r).then(() => {
-            console.log("Cached", event.request);
-          }).catch((error) => {
-            console.log("Failed caching", error, error.code, error.message, error.name);
-          });
-        }).catch((error) => {
-          console.log("Error fetching", event.request, error);
-        });
-        return response;
-      }).catch((error) => {
-        return new Response('' + error.code + ': ' + error.message, {
-          status: 404,
-          statusText: 'Not found'
-        })
-      });
-    }
-  }));
 });
 
 self.addEventListener('message', (event) => {
